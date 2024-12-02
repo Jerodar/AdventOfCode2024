a3433d286cd62104c75f59760e4eb9d5a21cd20f
Jeroen Oostdam <jeroenoostdam@gmail.com>
2024-12-02 16:26:45 +0100

    Benchmarked my custom search and compare it to .Count and Linq

--- Day1.cs
+++ Day1.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2024Input;
+using System.Diagnostics;
 
 namespace AdventOfCode2024;
 
@@ -26,42 +27,62 @@ public static class Day1
         Console.WriteLine("The total distance is " + distanceSum);
         Console.WriteLine("");
         Console.WriteLine("Day 1 Part Two");
-
         var similarityScore = 0;
-
-        for (var i = 0; i < firstList.Count; i++)
+        
+        Console.WriteLine("Using .Count");
+        TimeSpan time = Time(() =>
         {
-            var similarityCount = 0;
-            Console.WriteLine("Similarities for : " + firstList[i]);
-            if (firstList[i] < secondList[i])
+            foreach (var firstItem in firstList)
             {
-                similarityCount += CountMatchesBackwards(firstList[i], i, secondList);
+                var similarityCount = secondList.Count(x => x == firstItem);
+                similarityScore += similarityCount * firstItem;
             }
-            else if (firstList[i] > secondList[i])
+        });
+        Console.WriteLine($"SimilarityScore {similarityScore} in {time.TotalMilliseconds} ms");
+        
+        similarityScore = 0;
+        Console.WriteLine("Using linq");
+        time = Time(() =>
+        {
+            similarityScore += (from firstItem in firstList let similarityCount = secondList.Count(x => x == firstItem) select similarityCount * firstItem).Sum();
+        });
+        Console.WriteLine($"SimilarityScore {similarityScore} in {time.TotalMilliseconds} ms");
+        
+        
+        similarityScore = 0;
+        Console.WriteLine("Using custom search");
+        time = Time(() =>
+        {
+            for (var i = 0; i < firstList.Count; i++)
             {
-                similarityCount += CountMatchesForward(firstList[i], i, secondList);
-            }
-            else
-            {
-                Console.WriteLine("search both directions for match");
-                similarityCount++;
-                similarityCount += CountMatchesForward(firstList[i], i, secondList);
-                similarityCount += CountMatchesBackwards(firstList[i], i, secondList);
-            }
+                var similarityCount = 0;
+                if (firstList[i] < secondList[i])
+                {
+                    similarityCount += CountMatchesBackwards(firstList[i], i, secondList);
+                }
+                else if (firstList[i] > secondList[i])
+                {
+                    similarityCount += CountMatchesForward(firstList[i], i, secondList);
+                }
+                else
+                {
+                    similarityCount++;
+                    similarityCount += CountMatchesForward(firstList[i], i, secondList);
+                    similarityCount += CountMatchesBackwards(firstList[i], i, secondList);
+                }
             
-            similarityScore += similarityCount * firstList[i];
-            Console.WriteLine($"{firstList[i]} SimilarityCount {similarityCount} SimilarityScore {similarityScore}");    
-        }
+                similarityScore += similarityCount * firstList[i];   
+            }
+        });
+        Console.WriteLine($"SimilarityScore {similarityScore} in {time.TotalMilliseconds} ms");
     }
 
     private static int CountMatchesForward(int target, int start, List<int> list)
     {
-        Console.WriteLine("search forward for match");
         var similarityCount = 0;
         var j = start + 1;
         while (j < list.Count && target >= list[j])
         {
-            Console.WriteLine($"{j} - {target} - {list[j]}");
             if (list[j] == target)
                 similarityCount++;
             j++;
@@ -71,16 +92,22 @@ public static class Day1
 
     private static int CountMatchesBackwards(int target, int start, List<int> list)
     {
-        Console.WriteLine("search forward for match");
         var similarityCount = 0;
         var j = start - 1;
         while (j >= 0 && target <= list[j])
         {
-            Console.WriteLine($"{j} - {target} - {list[j]}");
             if (list[j] == target)
                 similarityCount++;
             j--;
         }
         return similarityCount;
+    }
+
+    private static TimeSpan Time(Action action)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        action();
+        stopwatch.Stop();
+        return stopwatch.Elapsed;
     }
 }
