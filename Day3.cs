f18247929ed514753458b45cd8c4ffd96bfd8d27
Jeroen Oostdam <jeroenoostdam@gmail.com>
2024-12-03 08:08:18 +0100

    Day 3 part two solution

--- Day3.cs
+++ Day3.cs
@@ -7,6 +7,7 @@ public static class Day3
 {
     public static void Run()
     {
+        Console.WriteLine("Day 3 Part One");
         string corruptedText = PuzzleData.GetDay3Input();
         string pattern = @"mul\((\d+),(\d+)\)";
         
@@ -18,10 +19,35 @@ public static class Day3
         {
                 int firstNumber = int.Parse(match.Groups[1].Value);
                 int secondNumber = int.Parse(match.Groups[2].Value);
-                Console.WriteLine($"{firstNumber} * {secondNumber}");
                 resultSum += firstNumber * secondNumber;
                 
                 match = match.NextMatch();
+        }
+        Console.WriteLine($"result: {resultSum}");
+        
+        Console.WriteLine("");
+        Console.WriteLine("Day 3 Part Two");
+        pattern = @"don't\(\)|do\(\)|mul\((\d+),(\d+)\)";
+        
+        regex = new Regex(pattern);
+        
+        match = regex.Match(corruptedText);
+        resultSum = 0;
+        bool mulEnabled = true;
+        while (match.Success)
+        {
+            if (match.Value == "don't()")
+                mulEnabled = false;
+            else if (match.Value == "do()")
+                mulEnabled = true;
+            else if (mulEnabled)
+            {
+                int firstNumber = int.Parse(match.Groups[1].Value);
+                int secondNumber = int.Parse(match.Groups[2].Value);
+                resultSum += firstNumber * secondNumber;
+            }
+                
+            match = match.NextMatch();
         }
         Console.WriteLine($"result: {resultSum}");
     }
