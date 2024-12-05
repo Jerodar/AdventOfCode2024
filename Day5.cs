d529b482508d0cd143183e70b8ed906f0c1ed407
Jeroen Oostdam <jeroenoostdam@gmail.com>
2024-12-05 10:29:51 +0100

    Day 5 part two solution

--- Day5.cs
+++ Day5.cs
@@ -9,31 +9,64 @@ public static class Day5
     {
         Console.WriteLine($"Day 5 Part One");
         List<int>[] manuals = PuzzleData.GetDay5Manuals();
-        List<(int,int)> rules = PuzzleData.GetDay5Rules();
+        List<(int, int)> rules = PuzzleData.GetDay5Rules();
 
         int middleSum = 0;
+        List<List<int>> correctedManuals = new();
         foreach (var manual in manuals)
         {
-            bool isInvalid = false;
-            for (int i = manual.Count - 2; i >= 0; i--)
+            var brokenRules = CheckForBrokenRules(rules, manual);
+            if (!brokenRules.Any())
             {
-                int numberToCheck = manual[i];
-                List<int> numbersAfter = manual.GetRange(i + 1, manual.Count - i - 1);
-                var brokenRules = (from rule in rules where rule.Item2 == numberToCheck && numbersAfter.Contains(rule.Item1) select rule).ToList();
-                if (brokenRules.Any())
-                {
-                    isInvalid = true;
-                    Console.WriteLine($"Manual has invalid number: number:{manual[i]} afer:{string.Join(", ", numbersAfter)} full:{string.Join(", ", manual)}");
-                    Console.WriteLine($"Broken rules: {string.Join(", ", brokenRules)}");
-                }
-                if(isInvalid) { break; }
+                int middle = (manual.Count - 1) / 2;
+                middleSum += manual[middle];
+                continue;
             }
-            if (isInvalid) { continue; }
-            
-            int middle = (manual.Count - 1) / 2;
-            middleSum += manual[middle];
+
+            List<int> correctedManual = new();
+            while (brokenRules.Any())
+            {
+                correctedManual = FixManual(manual, brokenRules);
+                brokenRules = CheckForBrokenRules(rules, correctedManual);
+            }
+            correctedManuals.Add(correctedManual);
         }
 
         Console.WriteLine(middleSum);
+
+        Console.WriteLine("");
+        Console.WriteLine($"Day 5 Part Two");
+
+        middleSum = 0;
+        foreach (var manual in correctedManuals)
+        {
+            int middle = (manual.Count - 1) / 2;
+            middleSum += manual[middle];
+        }
+        Console.WriteLine(middleSum);
+    }
+
+    private static List<int> FixManual(List<int> manual, List<(int, int)> brokenRules)
+    {
+        foreach (var brokenRule in brokenRules)
+        {
+            var indexA = manual.IndexOf(brokenRule.Item1);
+            var indexB = manual.IndexOf(brokenRule.Item2);
+            manual[indexA] = brokenRule.Item2;
+            manual[indexB] = brokenRule.Item1;
+        }
+
+        return manual;
+    }
+
+    private static List<(int, int)> CheckForBrokenRules(List<(int, int)> rules, List<int> manual)
+    {
+        List<(int, int)> brokenRules = new();
+        for (int i = manual.Count - 2; i >= 0; i--)
+        {
+            List<int> numbersAfter = manual.GetRange(i + 1, manual.Count - i - 1);
+            brokenRules.AddRange((from rule in rules where rule.Item2 == manual[i] && numbersAfter.Contains(rule.Item1) select rule).ToList());
+        }
+        return brokenRules;
     }
 }
