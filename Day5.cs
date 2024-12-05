64ef13594f64684b441deb07740324587fe6a46f
Jeroen Oostdam <jeroenoostdam@gmail.com>
2024-12-05 08:43:48 +0100

    Day 5 part one attempt

---
+++ Day5.cs
@@ -0,0 +1,39 @@
+using AdventOfCode2024;
+using AdventOfCode2024Input;
+
+namespace AdventOfCode2024;
+
+public static class Day5
+{
+    public static void Run()
+    {
+        Console.WriteLine($"Day 5 Part One");
+        List<int>[] manuals = PuzzleData.GetDay5Manuals();
+        List<(int,int)> rules = PuzzleData.GetDay5Rules();
+
+        int middleSum = 0;
+        foreach (var manual in manuals)
+        {
+            bool isInvalid = false;
+            for (int i = manual.Count - 2; i >= 0; i--)
+            {
+                int numberToCheck = manual[i];
+                List<int> numbersAfter = manual.GetRange(i + 1, manual.Count - i - 1);
+                var brokenRules = (from rule in rules where rule.Item2 == numberToCheck && numbersAfter.Contains(rule.Item1) select rule).ToList();
+                if (brokenRules.Any())
+                {
+                    isInvalid = true;
+                    Console.WriteLine($"Manual has invalid number: number:{manual[i]} afer:{string.Join(", ", numbersAfter)} full:{string.Join(", ", manual)}");
+                    Console.WriteLine($"Broken rules: {string.Join(", ", brokenRules)}");
+                }
+                if(isInvalid) { break; }
+            }
+            if (isInvalid) { continue; }
+            
+            int middle = (manual.Count / 2) + 1;
+            middleSum += manual[middle];
+        }
+
+        Console.WriteLine(middleSum);
+    }
+}
