04d8e3bc536e37286eb762c7046ece912498a4f4
Jeroen Oostdam <jeroenoostdam@gmail.com>
2024-12-06 12:53:20 +0100

    Day 6 part two solution

--- Day6.cs
+++ Day6.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.Design;
-using System.Runtime.CompilerServices;
 using AdventOfCode2024Input;
 
 namespace AdventOfCode2024;
@@ -16,80 +14,49 @@ public static class Day6
     
     public static void Run()
     {
-        Console.WriteLine($"Day 6 Part One");
-
+        Console.WriteLine("Day 6 Part One");
+        
         string[] map = PuzzleData.GetDay6Map();
+        
+        var path = new Dictionary<(int row, int col), Dir>();
+        var obstructions = new List<(int row, int col)>();
 
-        (int x, int y) position = GetGuardPos(map);
+        (int row, int col) startPosition = GetGuardPos(map);
+        var position = startPosition;
         var dir = Dir.Up;
-        var done = false;
-        var steps = new List<(int x, int y)>();
-        while (position.x >= 0 && position.y >= 0 && position.x < map.Count() && position.y < map[0].Count())
+
+        var nextPos = StepForward(position, dir);
+        while (nextPos.row >= 0 && nextPos.col >= 0 && nextPos.row < map.Length && nextPos.col < map[0].Length)
         {
-            if (map[position.x][position.y] == '#')
+            if (map[nextPos.row][nextPos.col] == '#')
             {
-                position = StepBack(position, dir);
-                dir = Turn(dir);
+                dir = TurnRight(dir);
             }
             else
             {
-                if (!steps.Contains(position))
-                    steps.Add(position);
-                position = StepForward(position, dir);
-            }
-        }
-        Console.WriteLine($"{steps.Count} steps");
+                path.TryAdd(position, dir);
         
-    }
+                // Part Two addition, simulate placing an obstruction in front and check if that creates a loop
+                if (!obstructions.Contains(nextPos) && !path.ContainsKey(nextPos) && nextPos != startPosition &&
+                    SimulateObstruction(map, path, position, dir))
+                {
+                    obstructions.Add(nextPos);
+                }
 
-    private static (int x, int y) StepForward((int x, int y) position, Dir dir)
-    {
-        switch (dir)
-        {
-            case Dir.Up:
-                return (position.x - 1, position.y);
-            case Dir.Right:
-                return (position.x, position.y + 1);
-            case Dir.Down:
-                return (position.x + 1, position.y);
-            case Dir.Left:
-            default:
-                return (position.x, position.y - 1);
+                position = nextPos;
+            }
+
+            nextPos = StepForward(position, dir);
         }
+        path.TryAdd(position, dir);
+        
+        Console.WriteLine($"{path.Count} steps");
+        
+        Console.WriteLine("");
+        Console.WriteLine("Day 6 Part Two");
+        Console.WriteLine($"{obstructions.Count} obstructions");
     }
-
-    private static (int x, int y) StepBack((int x, int y) position, Dir dir)
-    {
-        switch (dir)
-        {
-            case Dir.Up:
-                return (position.x + 1, position.y);
-            case Dir.Right:
-                return (position.x, position.y - 1);
-            case Dir.Down:
-                return (position.x - 1, position.y);
-            case Dir.Left:
-            default:
-                return (position.x, position.y + 1);
-        }
-    }
-
-    private static Dir Turn(Dir dir)
-    {
-        switch (dir)
-        {
-            case Dir.Up:
-                return Dir.Right;
-            case Dir.Right:
-                return Dir.Down;
-            case Dir.Down:
-                return Dir.Left;
-            case Dir.Left:
-            default:
-                return Dir.Up;
-        }
-    }
-
+    
     private static (int, int) GetGuardPos(string[] map)
     {
         var guardPos = (0, 0);
@@ -104,5 +71,70 @@ public static class Day6
         }
 
         return guardPos;
+    }
+    
+    private static Dir TurnRight(Dir dir)
+    {
+        switch (dir)
+        {
+            case Dir.Up:
+                return Dir.Right;
+            case Dir.Right:
+                return Dir.Down;
+            case Dir.Down:
+                return Dir.Left;
+            case Dir.Left:
+            default:
+                return Dir.Up;
+        }
+    }
+    
+    private static (int row, int col) StepForward((int row, int col) position, Dir dir)
+    {
+        switch (dir)
+        {
+            case Dir.Up:
+                return (position.row - 1, position.col);
+            case Dir.Right:
+                return (position.row, position.col + 1);
+            case Dir.Down:
+                return (position.row + 1, position.col);
+            case Dir.Left:
+            default:
+                return (position.row, position.col - 1);
+        }
+    }
+    
+    private static bool SimulateObstruction(string[] map, Dictionary<(int row, int col), Dir> path, (int row, int col) position, Dir dir)
+    {
+        // Simulates if placing an obstructing in front of the current position causes a loop
+        // If the while loop condition is false, it means the guard escaped and there was no loop
+        // If the path the guard takes is the same position and direction as previously, they are in a loop
+
+        var obstruction = StepForward(position, dir);
+        var simPath = new Dictionary<(int row, int col), Dir>();
+        
+        var nextPos = StepForward(position, dir);
+        while (nextPos.row >= 0 && nextPos.col >= 0 && nextPos.row < map.Length && nextPos.col < map[0].Length)
+        {
+            if (map[nextPos.row][nextPos.col] == '#' || nextPos == obstruction)
+            {
+                dir = TurnRight(dir);
+            }
+            else
+            {
+                if (path.ContainsKey(position) && path[position] == dir)
+                    return true;
+                if (simPath.ContainsKey(position) && simPath[position] == dir)
+                    return true;
+
+                simPath.TryAdd(position, dir);
+                position = nextPos;
+            }
+
+            nextPos = StepForward(position, dir);
+        }
+        
+        return false;
     }
 }
