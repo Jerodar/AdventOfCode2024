178c01015e39cdacf16b1caf789abee98ca1d0d8
Jeroen Oostdam <jeroenoostdam@gmail.com>
2024-12-09 09:51:41 +0100

    Day 9 part two solution

--- Day9.cs
+++ Day9.cs
@@ -1,29 +1,99 @@
+using System.Runtime.Intrinsics.Arm;
 using AdventOfCode2024Input;
 
 namespace AdventOfCode2024;
 
 public static class Day9
 {
+    private class File
+    {
+        public int ID = -1;
+        public int size = -1;
+        public int index = -1;
+    }
+    
     public static void Run()
     {
-        Console.WriteLine("Day 9 Part One");
-
         string driveMap = PuzzleData.GetDay9DriveMap();
         
+        Console.WriteLine("Day 9 Part One");
+
         int diskSize = driveMap.Sum(c => Convert.ToInt32(c.ToString()));
-        
         int[] resultMap = GetExpandedDriveMap(driveMap, diskSize);
-        
-        //PrintDriveMap(resultMap);
-        
         resultMap = CompressDriveMap(resultMap);
-        
-        //PrintDriveMap(resultMap);
-        
         long checkSum = CalculateCheckSum(resultMap);
+        Console.WriteLine(checkSum);
         
         Console.WriteLine();
+        Console.WriteLine("Day 9 Part Two");
+        
+        List<File> files = GetFiles(driveMap);
+        files = RearrangeFiles(files);
+        checkSum = CalculateFilesCheckSum(files);
         Console.WriteLine(checkSum);
+    }
+
+    private static List<File> GetFiles(string driveMap)
+    {
+        int fileId = 0;
+        int nextPos = 0;
+        bool isFile = true;
+        var results = new List<File>();
+        
+        foreach (char c in driveMap)
+        {
+            int fileSize = Convert.ToInt32(c.ToString());
+
+            if (isFile)
+            {
+                results.Add(new File { ID = fileId, size = fileSize, index = nextPos });
+                fileId++;
+            }
+            else
+            {
+                results.Add(new File { ID = -1, size = fileSize, index = nextPos });
+            }
+
+            nextPos += fileSize;
+            isFile = !isFile;
+        }
+
+        return results;
+    }
+    
+    private static List<File> RearrangeFiles(List<File> files)
+    {
+        for (int i = 0; i < files.Count; i++)
+        {
+            var file = files[i];
+            if (file.ID > -1) continue;
+
+            var insertIndex = file.index;
+            var gapSize = file.size;
+            while(gapSize > 0)
+            {
+                var fileToMove = (from target in files where target.ID > 0 && target.index > insertIndex && target.size <= gapSize orderby target.index descending select target).FirstOrDefault();
+                if (fileToMove == null) break;
+                fileToMove.index = insertIndex;
+                insertIndex += fileToMove.size;
+                gapSize -= fileToMove.size;
+            }
+        }
+        
+        return files;
+    }
+        
+    private static long CalculateFilesCheckSum(List<File> files)
+    {
+        long checkSum = 0;
+        foreach (File file in files)
+        {
+            if (file.ID == -1) continue;
+            var lastIndex = file.index + file.size;
+            for (int i = file.index; i < lastIndex; i++)
+                checkSum += file.ID * i;
+        }
+        return checkSum;
     }
 
     private static int[] GetExpandedDriveMap(string driveMap, int diskSize)
@@ -80,23 +150,5 @@ public static class Day9
             checkSum += (resultMap[i] * i);
         }
         return checkSum;
-    }
-
-    private static void PrintDriveMap(int[] driveMap)
-    {
-        string logMap = "";
-        foreach (int i in driveMap)
-        {
-            if (i != -1)
-            {
-                logMap += i;
-            }
-            else
-            {
-                logMap += ".";
-            }
-        }
-        Console.WriteLine();
-        Console.WriteLine(logMap);
     }
 }
