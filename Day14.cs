1d2125c355027647a59bc37c91cca7bb168c35b9
Jeroen Oostdam <jeroenoostdam@gmail.com>
2024-12-14 14:57:15 +0100

    Day 14 part two solution

--- Day14.cs
+++ Day14.cs
@@ -45,6 +45,25 @@ public static class Day14
         }
         var result = upRight * upLeft * downRight * downLeft;
         Console.WriteLine($"Safety factor: {result}");
+        Console.WriteLine();
+        Console.WriteLine("Day 14 Part Two");
+
+        robots = PuzzleData.GetDay14Robots();
+
+        for (var seconds = 0; seconds < 10000; seconds++)
+        {
+            for (var i = 0; i < robots.Count; i++)
+            {
+                robots[i] = MoveRobot(robots[i], maxRow, maxCol);
+            }
+
+            if (ContainsTriangle(robots))
+            {
+                Console.WriteLine($"Map after {seconds+1} seconds:");
+                DrawMap(robots, maxRow, maxCol); // check visually if this is a Christmas tree
+                Console.WriteLine();
+            }
+        }
     }
 
     private static (int px, int py, int vx, int vy)  MoveRobot((int px, int py, int vx, int vy) robot, int maxRow, int maxCol)
@@ -65,5 +84,36 @@ public static class Day14
         robot.py = newY;
 
         return robot;
+    }
+    
+    private static bool ContainsTriangle(List<(int px, int py, int vx, int vy)> robots)
+    {
+        var locations = new HashSet<(int x, int y)>();
+        foreach (var robot in robots)
+            locations.Add((robot.px, robot.py));
+        return robots.Any(robot => locations.Contains((robot.px - 2, robot.py)) &&
+                                            locations.Contains((robot.px - 1, robot.py)) &&
+                                            locations.Contains((robot.px - 1, robot.py + 1)) &&
+                                            locations.Contains((robot.px - 1, robot.py - 1)) &&
+                                            locations.Contains((robot.px, robot.py + 2)) &&
+                                            locations.Contains((robot.px, robot.py + 1)) &&
+                                            locations.Contains((robot.px, robot.py - 1)) &&
+                                            locations.Contains((robot.px, robot.py - 2)));
+    }
+    
+    private static void DrawMap(List<(int px, int py, int vx, int vy)> robots, int maxRow, int maxCol)
+    {
+        char[,] map = new char[maxRow, maxCol];
+        for (int row = 0; row < maxRow; row++)
+            for (int col = 0; col < maxCol; col++)
+                map[row, col] = '.';
+        foreach (var robot in robots)
+                map[robot.py, robot.px] = '#';
+        for (int row = 0; row < maxRow; row++)
+        {
+            for (int col = 0; col < maxCol; col++)
+                Console.Write(map[row, col]);
+            Console.WriteLine();
+        }
     }
 }
